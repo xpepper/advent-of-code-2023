20ece5119a3cc3db4ee6f07dc19401f002d7bd91
Pietro Di Bello <pierodibello@gmail.com>
2023-12-08 16:00:07 +0100

    refactor(day05): rename variable

--- src/main/kotlin/day05/LowestLocationGivenSeedPart2.kt
+++ src/main/kotlin/day05/LowestLocationGivenSeedPart2.kt
@@ -4,11 +4,12 @@ private val seeds: Sequence<LongRange> =
     "seeds: 1347397244 12212989 2916488878 1034516675 2821376423 8776260 2240804122 368941186 824872000 124877531 1597965637 36057332 4091290431 159289722 1875817275 106230212 998513229 159131132 2671581775 4213184"
         .split(":")[1].toLongs()
         .zipWithNext().filterIndexed { index, _ -> index % 2 == 0 }
-        .map { (start, rangeSize) -> start..start + rangeSize }.asSequence()
+        .map { (start, rangeSize) -> start..start + rangeSize }
+        .asSequence()
 
 fun main() {
-    seeds.flatMap { seed: LongRange ->
-        seed.asSequence().map {
+    seeds.flatMap { seedRange: LongRange ->
+        seedRange.asSequence().map {
             val seedToSoil = mapper(seedToSoilMap, it)
             val soilToFertilizer = mapper(soilToFertilizerMap, seedToSoil)
             val fertilizerToWater = mapper(fertilizerToWaterMap, soilToFertilizer)
