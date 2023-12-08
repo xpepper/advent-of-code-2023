2e0676f79b3f7b5cf6769726fa607b033216dbdc
Pietro Di Bello <pierodibello@gmail.com>
2023-12-08 02:00:42 +0100

    feat(day05): working on part 2...

--- src/main/kotlin/day05/LowestLocationGivenSeedPart2.kt
+++ src/main/kotlin/day05/LowestLocationGivenSeedPart2.kt
@@ -1,16 +1,11 @@
 package day05
 
-data class ConversionMap(val destinationRangeStart: Long, val sourceRangeStart: Long, val rangeLength: Long)
-
-fun mapper(conversionMaps: List<ConversionMap>, input: Long): Long =
-    conversionMaps.find { input in it.sourceRangeStart until it.sourceRangeStart + it.rangeLength }
-        ?.let { input - (it.sourceRangeStart - it.destinationRangeStart) }
-        ?: input
-
 private val seeds =
     "seeds: 1347397244 12212989 2916488878 1034516675 2821376423 8776260 2240804122 368941186 824872000 124877531 1597965637 36057332 4091290431 159289722 1875817275 106230212 998513229 159131132 2671581775 4213184"
         .split(":")[1].toLongs()
-
+        .zipWithNext().filterIndexed { index, _ -> index % 2 == 0 }
+        .map { (start, rangeSize) -> start..start + rangeSize }
+        .also(::println)
 
 fun main() {
     seeds.mapIndexed { index, seed ->
