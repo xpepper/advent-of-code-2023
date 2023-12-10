ffb7944ae25bebac3d59616325422623ed830dd5
Pietro Di Bello <pierodibello@gmail.com>
2023-12-10 23:42:37 +0100

    refactor(day06): simplify code

--- src/main/kotlin/day06/NumberOfWaysToBeatTheRecord.kt
+++ src/main/kotlin/day06/NumberOfWaysToBeatTheRecord.kt
@@ -6,20 +6,18 @@ private val puzzleInput = """
 """.trimIndent()
 
 fun main() {
-    val numberOfWaysToBeatTheRecord = mutableListOf<Int>()
     val (times, distances) = puzzleInput.split("\n")
         .map { it.split(":")[1].trim().split(Regex("\\s+")).map(String::toInt) }
-    times.zip(distances).forEach { (raceTime, raceRecord) ->
+    times.zip(distances).map { (raceTime, raceRecord) ->
         var counter = 0
         for (chargeTime in 0..raceTime) {
             if (((raceTime - chargeTime) * chargeTime) > raceRecord) {
                 counter++
             }
         }
-        numberOfWaysToBeatTheRecord.add(counter)
         println("Record beaten $counter times")
-    }
-    numberOfWaysToBeatTheRecord.product().also(::println)
+        counter
+    }.product().also(::println)
 }
 
 private fun Iterable<Int>.product(): Int = fold(1) { acc, i -> acc * i }
