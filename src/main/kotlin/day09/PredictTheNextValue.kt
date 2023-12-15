ce409177141cb5c25776fde37f2b3948bed73896
Pietro Di Bello <pierodibello@gmail.com>
2023-12-15 22:21:53 +0100

    refactoring(day08): simplify code

--- src/main/kotlin/day09/PredictTheNextValue.kt
+++ src/main/kotlin/day09/PredictTheNextValue.kt
@@ -207,16 +207,10 @@ fun main() {
     val input = inputData.split("\n").asSequence()
         .map { inputLine -> inputLine.trim().split(" ").map { it.trim().toInt() } }
 
-    val result = input.map { sequence ->
+    val result = input.map { sequence: List<Int> ->
         generateSequence(sequence) { seq ->
             seq.windowed(2) { it[1] - it[0] }.takeIf { it.any { it != 0 } }
-        }.toList().let { seqList ->
-            var acc = 0
-            for (i in seqList.indices.reversed()) {
-                acc += seqList[i].last()
-            }
-            acc
-        }
+        }.sumOf { sequences -> sequences[sequences.indices.last()] }
     }.sum()
 
     println(result)
