d773d33bf813bd46da198cd0781f9bf690e24899
Pietro Di Bello <pierodibello@gmail.com>
2023-12-15 23:13:20 +0100

    refactor(day08)

--- src/main/kotlin/day09/PredictThePreviousAndTheNextValue.kt
+++ src/main/kotlin/day09/PredictThePreviousAndTheNextValue.kt
@@ -4,15 +4,11 @@ fun main() {
     val input = inputData.split("\n").asSequence()
         .map { inputLine -> inputLine.trim().split(" ").map { it.trim().toInt() } }
 
-    val result = input.map { sequence ->
+    val result = input.map { sequence: List<Int> ->
         generateSequence(sequence) { seq ->
             seq.windowed(2) { it[1] - it[0] }.takeIf { it.any { it != 0 } }
         }.toList().let { seqList ->
-            var acc = 0
-            for (i in seqList.indices.reversed()) {
-                acc = seqList[i].first() - acc
-            }
-            acc
+            seqList.indices.reversed().fold(0) { acc, each -> seqList[each].first() - acc }
         }
     }.sum()
 
